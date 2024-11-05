ecb94f1c50d7b09ef850751a812a315e83d72401
Tom <tom@MacBook-Air-Tom.local>
2024-11-05 19:08:32 +0100

    obnulay

--- app/src/config.ts
+++ app/src/config.ts
@@ -10,199 +10,46 @@ export default defineConfig({
         {
           media: {
             type: 'sticker',
-            src: import('./assets/stickers/duck_hello.tgs'),
-            size: 250,
+            src: import('./assets/stickers/yoda_thinking.tgs'),
+            size: 200,
           },
           shape: 'square',
           pagination: 'count',
-          title: 'Welcome to BeskarGram Onboarding',
+          title: 'All-in-one AI tool for a successfull job search ',
           description: `
-            BeskarGram — это ваш AI консьерж сервис для быстрого найма и поиска работы .
-            <br><br>
-            <b>Для клиентов:</b> Найдите и выберите лучших IT-специалистов, не тратя время на долгий поиск. Мы предоставляем доступ к квалифицированным разработчикам, их актуальным резюме и видео-самопрезентациям — всё это в одном приложении!
-            <br><br>
-            <b>Для разработчиков:</b> Продемонстрируйте свои навыки и уникальность прямо здесь! BeskarGram помогает вам рассказать о себе через резюме и видео, а наш ИИ автоматизирует подачу на подходящие вакансии, позволяя сосредоточиться на развитии.
-            <br><br>
-            BeskarGram — это <b>быстро</b>, <b>удобно</b> и <b>прямо к цели</b>. Давайте начнем!
+            This bot is all you need for the next level job search.
           `,
-          button: 'Дальше',
-        },
-
-        // image
-        {
-          media: {
-            type: 'image',
-            src: import('./assets/img/durov.webp'),
-          },
-          shape: 'rounded',
-          pagination: 'count',
-          title: 'Onboarding supports many types of content',
-          description:
-            "Here you can see <b>Image</b>. But it's just the beginning...",
           button: 'Next',
         },
 
-        // sticker
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_love.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Telegram stickers',
-          description:
-            'Just download any <b>.tgs</b> sticker from Telegram and use it in your onboardings',
-          button: 'Next',
-        },
-
-        // form
-        {
-          extends: 'form', // note, it's important to extend from 'form' here
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_spy.tgs'),
-            size: 150,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Forms',
-          description: 'User fills in the form – the bot receives the data',
-          form: [
-            {
-              id: 'text_from_form',
-              placeholder: 'Text input',
-              type: 'text',
-            },
-            {
-              id: 'number_from_form',
-              placeholder: 'Number input',
-              type: 'number',
-            },
-            {
-              id: 'checkbox_from_form',
-              placeholder: 'Checkbox',
-              type: 'checkbox',
-            },
-          ],
-          button: 'Next',
-        },
-
-        // video
         {
           media: {
             type: 'video',
-            src: import('./assets/videos/spongebob.mp4'),
-            poster: import('./assets/img/spongebob_poster.webp'),
-            style: 'aspect-ratio: 400/287', // here we manually set video aspect-ratio (default is 16:9)
+            src: import('./assets/videos/chatgpt_1.mp4'),
+            poster: import('./assets/img/chatgpt_1_poster.webp'),
+            style: 'aspect-ratio: 780/936',
           },
           shape: 'rounded',
           pagination: 'count',
-          title: 'Videos',
-          description:
-            "Typically, video starts <b>automatically</b><br><br>However, on iOS, it will only autoplay upon any prior tap on the page ('Next' button doesn't count). If video doesn't autoplay, user will see preview and pretty animation, inviting them to tap to play the video",
+          title: 'Ask any questions',
+          description: "Get instant smart answers",
           button: 'Next',
         },
 
-        // list
+        // video 2
         {
           media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_juggling.tgs'),
-            size: 150,
+            type: 'video',
+            src: import('./assets/videos/chatgpt_2.mp4'),
+            poster: import('./assets/img/chatgpt_2_poster.webp'),
+            style: 'aspect-ratio: 780/936',
           },
-          shape: 'square',
+          shape: 'rounded',
           pagination: 'count',
-          title: 'Lists',
-          description:
-            'Lists can be used to showcase <b>features</b> of your product. Items support customizable icons',
-          list: [
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/guide.svg'),
-                size: 30,
-              },
-              text: 'Some cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/track.svg'),
-                size: 30,
-              },
-              text: 'Some very cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/time.svg'),
-                size: 30,
-              },
-              text: 'Some extremely cool feature',
-            },
-          ],
-          button: 'Next',
-        },
-
-        // "everything is customizable" slide
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_xray.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Everything is customizable',
-          description: '',
-          textAlign: 'center',
-          list: [
-            '<b>CSS styles</b>: extend primary colors from Telegram or set yours',
-            'Button text and actions (look down)',
-            'Use our carefully crafted <b>presets</b> or easily create your own',
-          ],
-          button: 'Super-Duper Next',
-        },
-
-        // slide with other features
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_cool.tgs'),
-            size: 150,
-          },
-          shape: 'square',
-          pagination: 'count',
-          title: 'Some other features:',
-          description: '',
-          list: [
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-            "And many more... (see <a href='https://github.com/Easterok/telegram-onboarding-kit' target='_blank'>GitHub</a>)",
-          ],
-          button: 'Next',
-        },
-
-        // go to paywall slide
-        {
-          media: {
-            type: 'sticker',
-            src: import('./assets/stickers/duck_knife.tgs'),
-            size: 250,
-          },
-          shape: 'square',
-          pagination: 'count',
-          textAlign: 'center',
-          title: 'But onboarding slides are not enough...',
-          description: "Let's go to Paywall",
+          title: 'Send Voice Messages',
+          description: "90+ languages are supported",
           button: {
-            content: 'Go to Paywall',
+            content: 'Next',
             to: '/paywall',
           },
         },
@@ -215,42 +62,41 @@ export default defineConfig({
       path: '/paywall',
       media: {
         type: 'sticker',
-        src: import('./assets/stickers/duck_money.tgs'),
+        src: import('./assets/stickers/yoda_heart.tgs'),
         size: 150,
       },
       shape: 'square',
-      title: 'Your beautiful Paywall',
+      title: 'Unlock PRO features',
       list: [
-        'Adjustable product cards',
-        '<b>👛 Wallet Pay</b> and <b>Telegram Payments</b> ready. Add custom methods easily',
-        'Subscriptions or One-time payments',
+        '<b>100,000</b> tokens/day (50+ A4-pages for ChatGPT)',
+        'Access to <b>GPT-4</b>',
+        'Unlock PRO chat modes and voice messages',
       ],
       products: [
         {
+          id: '1_week_subscription',
+          title: '1 week subscription',
+          description: '2.49$/week',
+          discount: '',
+          price: 2.49,
+        },
+        {
           id: '1_month_subscription',
           title: '1 month subscription',
-          description: '2$/month',
-          discount: '',
-          price: 2,
+          description: '1.35$/week',
+          discount: '45% discount',
+          price: 5.99,
         },
         {
           id: '1_year_subscription',
           title: '1 year subscription',
-          description: '1$/month',
-          discount: 'Discount 50%',
-          price: 12,
-        },
-        {
-          id: 'lifetime_access',
-          title: 'Lifetime access',
-          description: '20$ once',
-          discount: 'Best offer',
-          price: 20,
+          description: '0.96$/week',
+          discount: '60% discount (best offer)',
+          price: 49.99,
         },
       ],
       mainButtonText: 'Buy for {price}',
-      popup: {
-        // popup for payment methods choice
+      popup: {  // popup for payment methods choice
         type: 'web',
       },
       links: [
@@ -264,5 +110,5 @@ export default defineConfig({
         },
       ],
     },
-  ],
+  ]
 });
